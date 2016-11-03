84f3c707194af101b58b03a380b93ecdfe03014d
Gary McGhee <gary@buzzware.com.au>
2016-11-03 10:24:44 +0800

    shared tests running

---
+++ SqliteTests/TestsSample.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace SqliteTestsDroid
+{
+	[TestFixture]
+	public class TestsSample
+	{
+
+		[SetUp]
+		public void Setup() { }
+
+
+		[TearDown]
+		public void Tear() { }
+
+		[Test]
+		public void Pass()
+		{
+			Debug.WriteLine("test1");
+			Assert.True(true);
+		}
+
+		[Test]
+		public void Fail()
+		{
+			Assert.False(true);
+		}
+
+		[Test]
+		[Ignore("another time")]
+		public void Ignore()
+		{
+			Assert.True(false);
+		}
+
+		[Test]
+		public void Inconclusive()
+		{
+			Assert.Inconclusive("Inconclusive");
+		}
+	}
+}
