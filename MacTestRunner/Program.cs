df8db18b109e733b514cc10fced512df6982466b
Gary McGhee <gary@buzzware.com.au>
2016-11-03 11:53:02 +0800

    added osx sqlite injection

--- MacTestRunner/Program.cs
+++ MacTestRunner/Program.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Reflection;
+using GalaSoft.MvvmLight.Ioc;
 using NUnit.Common;
 using NUnitLite;
+using SQLite;
 
 namespace NUnitLite.Tests
 {
@@ -9,6 +11,17 @@ namespace NUnitLite.Tests
     {
         public static int Main(string[] args)
         {
+
+					string databaseFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+					string dbFilePath = databaseFolder + "/MacTestRunner.db";
+					//File folder = new File(databaseFolder);
+					//if (!folder.Exists())
+					//	folder.Mkdir();
+					SQLitePCL.Batteries.Init();
+					var conn = new SQLite.SQLiteConnection(dbFilePath);
+					SimpleIoc.Default.Register<SQLiteConnection>(() => conn);
+
+
 					args = new string[] { "--verbose" };
 					var writer = new ExtendedTextWrapper(Console.Out);
 					var assembly = Assembly.GetExecutingAssembly();
